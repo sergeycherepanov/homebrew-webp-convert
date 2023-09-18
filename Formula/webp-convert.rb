8d3b271acd250920372c3370c1cda2b430cfe33b
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-09-18 21:31:19 +0400

    0.1.4

--- Formula/webp-convert.rb
+++ Formula/webp-convert.rb
@@ -2,15 +2,14 @@ require 'formula'
 
 class WebpConvert < Formula
   url "https://github.com/sergeycherepanov/homebrew-webp-convert.git", :using => :git
-  version "0.1.3"
+  version "0.1.4"
   revision 1
 
   depends_on 'webp'
   depends_on 'rush-parallel'
 
   def install
-    libexec.install Dir["bin"]
-    bin.write_exec_script "bin/webp-convert"
+    bin.install "bin/webp-convert"
   end
 
   def caveats
